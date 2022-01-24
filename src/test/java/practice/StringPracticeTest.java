bbd6b39b629f5c73a9f405875fe03518814f4e1c
LeeJeongGi <apple4rhk@naver.com>
2022-01-24 22:35:51 +0900

    refactor : String 테스트 클래스 이름 변경

--- src/test/java/practice/StringPracticeTest.java
+++ src/test/java/practice/StringPracticeTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class practiceTest {
+public class StringPracticeTest {
 
     @Test
     public void split_Test() {
